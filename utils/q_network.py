b9b8aff77f55ca4732d61df04285cbbd8b1e5b6f
Logan Lambeth <LoganLambeth8@gmail.com>
2024-11-26 14:42:36 -0500

    Network documentation

--- utils/q_network.py
+++ utils/q_network.py
@@ -41,6 +41,7 @@ class QNetwork(nn.Module):
     
     def __init__(self, state_size, action_size, hidden_layers=[128, 64]):
         super(QNetwork, self).__init__()
+        # The shared part of the network processes the state input to extract features.
         self.feature = nn.Sequential(
             nn.Linear(state_size, hidden_layers[0]),
             #nn.BatchNorm1d(hidden_layers[0]),
@@ -51,6 +52,8 @@ class QNetwork(nn.Module):
         )
         
         # Value stream
+        # This stream estimates the value of the state, which is a scalar representing the expected 
+        # return of being in the current state regardless of the action taken.
         self.value_stream = nn.Sequential(
             nn.Linear(hidden_layers[1], 32),
             #nn.BatchNorm1d(32),
@@ -59,6 +62,9 @@ class QNetwork(nn.Module):
         )
         
         # Advantage stream
+        # This stream estimates the advantage of each action, which represents 
+        # how much better or worse an action is compared to others in a given state.
+
         self.advantage_stream = nn.Sequential(
             nn.Linear(hidden_layers[1], 32),
             #nn.BatchNorm1d(32),
